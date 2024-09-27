c82fbb95b3bd6cf1c35b435d354ae1dc97db306e
shankar7042 <shankar.kumar.4171@gmail.com>
2024-09-28 00:33:41 +0530

    load config file

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/shankar7042/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// load config
+
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to golang crud api"))
+	})
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.HttpServer.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started at %s", cfg.HttpServer.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
 }
