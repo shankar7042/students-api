aab91567d557c73fc29581c94302c2421412d187
shankar7042 <shankar.kumar.4171@gmail.com>
2024-10-01 08:10:55 +0530

    database connection

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/shankar7042/students-api/internal/config"
 	"github.com/shankar7042/students-api/internal/http/student"
+	"github.com/shankar7042/students-api/internal/storage/sqlite"
 )
 
 func main() {
@@ -20,6 +21,13 @@ func main() {
 	cfg := config.MustLoad()
 
 	// database setup
+
+	_, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("Storage Initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
 	// setup router
 
 	router := http.NewServeMux()
---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,35 @@
+package sqlite
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/shankar7042/students-api/internal/config"
+)
+
+type Sqlite struct {
+	Db *sql.DB
+}
+
+func New(cfg *config.Config) (*Sqlite, error) {
+	db, err := sql.Open("sqlite3", cfg.StoragePath)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS students(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT,
+		email TEXT,
+		age INTEGER
+	)`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Sqlite{
+		Db: db,
+	}, nil
+
+}
